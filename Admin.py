1ae0a3f175a5c352b468523a64fd282db10f99b1
Maxim Lisovskiy <chinatri@gmail.com>
2024-02-15 14:18:43 +0200

    Added the function of choosing a specialty

--- Admin.py
+++ Admin.py
@@ -1,1 +1,8 @@
-print("Hello world")
+def Choose_the_option():
+    option = int(
+        input("Вибери спеціальність: \n" "1. Адмін\n" "2. Менеджер\n" "3. Покупець\n")
+    )
+    return option
+
+
+option = Choose_the_option()
