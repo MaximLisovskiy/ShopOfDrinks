1a035b00e7d7bb67d50ca89a709a3a4102fe8b43
Maxim Lisovskiy <chinatri@gmail.com>
2024-02-20 12:49:52 +0200

    Update tests

--- tests/test_admin_functions.py
+++ tests/test_admin_functions.py
@@ -2,8 +2,8 @@ from admin import *
 
 
 def test_add_drink():
-    drinks = {}
-    title = "Lager"
+    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+    title = "Obolon"
     price = 34
     add_drink(drinks, title, price)
 
@@ -11,24 +11,22 @@ def test_add_drink():
     assert drinks[title] == price
 
 
+
 def test_remove_drink():
-    drinks = {}
-    title = "Lager"
-    price = 40
-    add_drink(drinks, title, price)
+    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+    title = "Горілка-Хортиця"
     remove_drink(drinks, title)
 
     assert title not in drinks
 
 
 def test_change_price_drinks():
-    drinks = {}
-    title = "Lager"
-    price = 40
+    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+    title = "Лагер"
     new_price = 50
-    add_drink(drinks, title, price)
     change_price_drink(drinks, title, new_price)
 
+    assert title in drinks
     assert drinks[title] == new_price
 
 
@@ -42,42 +40,31 @@ def test_add_employee():
     assert dict_employees[employee] == salary
 
 def test_remove_employee():
-    dict_employees = {}
+    dict_employees = {"Manager": 500}
     employee = "Manager"
-    salary = 4500
-    add_employee(dict_employees, employee, salary)
     remove_employee(dict_employees, employee)
 
     assert employee not in dict_employees
 
 
 def test_change_salary_of_employee():
-    dict_employees = {}
+    dict_employees = {"Manager": 4500}
     employee = "Manager"
-    salary = 4500
     new_salary = 5000
-    add_employee(dict_employees, employee, salary)
     change_salary_of_employee(dict_employees, employee, new_salary)
 
     assert dict_employees[employee] == new_salary
 
 
 def test_show_employee():
-    dict_employees = {}
-    employee = "Manager"
-    salary = 4500
-    add_employee(dict_employees, employee, salary)
+    dict_employees = {"Manager": 4500}
     show_employees(dict_employees)
 
-    assert show_employees(dict_employees) == print(dict_employees)
-
+    assert print(show_employees(dict_employees)) == print(dict_employees)
 
 def test_show_drinks():
-    drinks = {}
-    name = "Lager"
-    price = 34
-    add_drink(drinks, name, price)
+    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
     show_drinks(drinks)
 
-    assert show_drinks(drinks) == print(drinks)
+    assert print(show_drinks(drinks)) == print(drinks)
 
