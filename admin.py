166e85228a8dd81ca04a13da412a33156f302c0e
Maxim Lisovskiy <chinatri@gmail.com>
2024-02-21 18:37:54 +0200

    Update test but they not work, help please

--- admin.py
+++ admin.py
@@ -1,38 +1,38 @@
-def add_drink(drinks: dict, title: str, price: int):
-    if title not in drinks:
-        drinks[title] = price
+class Admin:
+    def __init__(self):
+        self.drinks = {}
+        self.dict_employees = {}
 
+    def add_drink(self, drinks: dict, title: str, price: int):
+        if title not in self.drinks:
+            self.drinks[title] = price
 
-def remove_drink(drinks: dict, title: str):
-    if title in drinks:
-        drinks.pop(title)
+    def remove_drink(self, drinks: dict, title: str):
+        if title in self.drinks:
+            self.drinks.pop(title)
 
+    def change_price_drink(self, drinks: dict, title: str, new_price: int):
+        for title in self.drinks.keys():
+            if title in drinks:
+                self.drinks[title] = new_price
 
-def change_price_drink(drinks: dict, title: str, new_price: int):
-    for title in drinks.keys():
-        if title in drinks:
-            drinks[title] = new_price
+    def add_employee(self, dict_employees: dict, employee: str, salary: int):
+        self.dict_employees[employee] = salary
 
+    def remove_employee(self, dict_employees: dict, employee: str):
+        if employee in self.dict_employees:
+            self.dict_employees.pop(employee)
 
-def add_employee(dict_employees: dict, employee: str, salary: int):
-    dict_employees[employee] = salary
+    def change_salary_of_employee(
+        self, dict_employee: dict, employee: str, new_salary: int
+    ):
+        for employee in self.dict_employees.keys():
+            if employee in dict_employee:
+                self.dict_employees[employee] = new_salary
 
+    def show_employees(self, dict_employees: dict):
+        return self.dict_employees
 
-def remove_employee(dict_employees: dict, employee: str):
-    if employee in dict_employees:
-        dict_employees.pop(employee)
-
-
-def change_salary_of_employee(dict_employee: dict, employee: str, new_salary: int):
-    for employee in dict_employee.keys():
-        if employee in dict_employee:
-            dict_employee[employee] = new_salary
-
-
-def show_employees(dict_employees: dict):
-    return dict_employees
-
-
-def show_drinks(drinks: dict):
-    return drinks
+    def show_drinks(self, drinks: dict):
+        return self.drinks
 
--- tests/test_admin_functions.py
+++ tests/test_admin_functions.py
@@ -1,83 +1,95 @@
-from admin import *
+from admin import Admin
+import unittest
 
 
-def test_add_drink():
-    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
-    title = "Obolon"
-    price = 34
-    add_drink(drinks, title, price)
-
-    assert title in drinks
-    assert drinks[title] == price
-    assert type(drinks) == dict
-    assert type(title) == str
-    assert type(price) == int
-
-def test_remove_drink():
-    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
-    title = "Горілка-Хортиця"
-    remove_drink(drinks, title)
-
-    assert title not in drinks
-    assert type(drinks) == dict
-    assert type(title) == str
+class TestAdminFunction(unittest.TestCase):
 
 
-def test_change_price_drinks():
-    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
-    title = "Лагер"
-    new_price = 50
-    change_price_drink(drinks, title, new_price)
+    def test_add_drink(self):
+        admin = Admin()
+        drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+        title = "Obolon"
+        price = 34
+        admin.add_drink(drinks, title, price)
 
-    assert title in drinks
-    assert drinks[title] == new_price
-    assert type(drinks) == dict
-    assert type(title) == str
-    assert type(new_price) == int
+        assert title in drinks
+        assert drinks[title] == price
+        assert type(drinks) == dict
+        assert type(title) == str
+        assert type(price) == int
 
-def test_add_employee():
-    dict_employees = {}
-    employee = "Manager"
-    salary = 4500
-    add_employee(dict_employees, employee, salary)
+    def test_remove_drink(self):
+        admin = Admin()
+        drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+        title = "Горілка-Хортиця"
+        admin.remove_drink(drinks, title)
 
-    assert employee in dict_employees
-    assert dict_employees[employee] == salary
-    assert type(employee) == str
-    assert type(salary) == int
-    assert type(dict_employees) == dict
-
-
-def test_remove_employee():
-    dict_employees = {"Manager": 500}
-    employee = "Manager"
-    remove_employee(dict_employees, employee)
-
-    assert employee not in dict_employees
-    assert type(employee) == str
-    assert type(dict_employees) == dict
+        assert title not in drinks
+        assert type(drinks) == dict
+        assert type(title) == str
 
 
-def test_change_salary_of_employee():
-    dict_employees = {"Manager": 4500}
-    employee = "Manager"
-    new_salary = 5000
-    change_salary_of_employee(dict_employees, employee, new_salary)
+    def test_change_price_drinks(self):
+        admin = Admin()
+        drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+        title = "Лагер"
+        new_price = 50
+        admin.change_price_drink(drinks, title, new_price)
 
-    assert dict_employees[employee] == new_salary
-    assert type(dict_employees) == dict
-    assert type(employee) == str
-    assert type(new_salary) == int
+        assert title in drinks
+        assert drinks[title] == new_price
+        assert type(drinks) == dict
+        assert type(title) == str
+        assert type(new_price) == int
 
-def test_show_employee():
-    dict_employees = {"Manager": 4500}
+    def test_add_employee(self):
+        admin = Admin()
+        dict_employees = {}
+        employee = "Manager"
+        salary = 4500
+        admin.add_employee(dict_employees, employee, salary)
 
-    assert show_employees(dict_employees) == dict_employees
-    assert type(dict_employees) == dict
+        assert employee in dict_employees
+        assert dict_employees[employee] == salary
+        assert type(employee) == str
+        assert type(salary) == int
+        assert type(dict_employees) == dict
 
 
-def test_show_drinks():
-    drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+    def test_remove_employee(self):
+        admin = Admin()
+        dict_employees = {"Manager": 500}
+        employee = "Manager"
+        admin.remove_employee(dict_employees, employee)
 
-    assert show_drinks(drinks) == drinks
-    assert type(drinks) == dict
+        assert employee not in dict_employees
+        assert type(employee) == str
+        assert type(dict_employees) == dict
+
+
+    def test_change_salary_of_employee(self):
+        admin = Admin()
+        dict_employees = {"Manager": 4500}
+        employee = "Manager"
+        new_salary = 5000
+        admin.change_salary_of_employee(dict_employees, employee, new_salary)
+
+        assert dict_employees[employee] == new_salary
+        assert type(dict_employees) == dict
+        assert type(employee) == str
+        assert type(new_salary) == int
+
+    def test_show_employee(self):
+        admin = Admin()
+        dict_employees = {"Manager": 4500}
+
+        assert admin.show_employees(dict_employees) == dict_employees
+        assert type(dict_employees) == dict
+
+
+    def test_show_drinks(self):
+        admin = Admin()
+        drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
+
+        assert admin.show_drinks(drinks) == drinks
+        assert type(drinks) == dict
