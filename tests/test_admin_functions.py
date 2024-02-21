1e7b334fabae9149e2b2ef0bfb5d06a156317ab1
Maxim Lisovskiy <chinatri@gmail.com>
2024-02-21 13:00:41 +0200

    Added tests to check data types

--- tests/test_admin_functions.py
+++ tests/test_admin_functions.py
@@ -9,7 +9,9 @@ def test_add_drink():
 
     assert title in drinks
     assert drinks[title] == price
-
+    assert type(drinks) == dict
+    assert type(title) == str
+    assert type(price) == int
 
 def test_remove_drink():
     drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
@@ -17,6 +19,8 @@ def test_remove_drink():
     remove_drink(drinks, title)
 
     assert title not in drinks
+    assert type(drinks) == dict
+    assert type(title) == str
 
 
 def test_change_price_drinks():
@@ -27,7 +31,9 @@ def test_change_price_drinks():
 
     assert title in drinks
     assert drinks[title] == new_price
-
+    assert type(drinks) == dict
+    assert type(title) == str
+    assert type(new_price) == int
 
 def test_add_employee():
     dict_employees = {}
@@ -37,6 +43,10 @@ def test_add_employee():
 
     assert employee in dict_employees
     assert dict_employees[employee] == salary
+    assert type(employee) == str
+    assert type(salary) == int
+    assert type(dict_employees) == dict
+
 
 def test_remove_employee():
     dict_employees = {"Manager": 500}
@@ -44,6 +54,8 @@ def test_remove_employee():
     remove_employee(dict_employees, employee)
 
     assert employee not in dict_employees
+    assert type(employee) == str
+    assert type(dict_employees) == dict
 
 
 def test_change_salary_of_employee():
@@ -53,15 +65,19 @@ def test_change_salary_of_employee():
     change_salary_of_employee(dict_employees, employee, new_salary)
 
     assert dict_employees[employee] == new_salary
-
+    assert type(dict_employees) == dict
+    assert type(employee) == str
+    assert type(new_salary) == int
 
 def test_show_employee():
     dict_employees = {"Manager": 4500}
 
     assert show_employees(dict_employees) == dict_employees
+    assert type(dict_employees) == dict
+
 
 def test_show_drinks():
     drinks = {"Лагер": 30, "Горілка-Хортиця": 180}
 
     assert show_drinks(drinks) == drinks
-
+    assert type(drinks) == dict
